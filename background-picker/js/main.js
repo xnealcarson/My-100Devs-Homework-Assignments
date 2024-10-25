94a52b85553a5a25b42f6a116a5d023313ad78fb
xnealcarson <xavier.f.nealcarson@gmail.com>
2024-10-24 20:50:49 -0500

    Add files via upload

---
+++ background-picker/js/main.js
@@ -0,0 +1,110 @@
+/*document.getElementById('purple').onclick = partyPurple
+document.getElementById('green').onclick = partyGreen
+document.getElementById('blue').onclick = partyBlue
+
+
+function partyPurple() {
+  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+  document.querySelector('body').style.color = 'white'
+}
+function partyGreen() {
+  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
+  document.querySelector('body').style.color = 'white'
+}
+function partyBlue() {
+  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
+  document.querySelector('body').style.color = 'white'
+}*/
+
+
+/*document.getElementById ('purple').onclick = partyPurple
+function partyPurple() {
+  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+  document.querySelector('body').style.color = 'white'
+}
+
+document.getElementById ('green').onclick = partyGreen
+function partyGreen () {
+  document.querySelector ('body').style.backgroundColor = 'rgba(0,253,81,1)'
+  document.querySelector ('body').style.color = 'white'
+}
+
+document.getElementById ('blue').onclick = partyBlue
+function partyBlue(){
+  document.querySelector ('body').style.backgroundColor = 'rgba(0,254,255)'
+  document.querySelector ('body').style.color = white
+}*/
+
+/*document.getElementById ('purple').onclick = partyPurple 
+function partyPurple() {
+document.querySelector ('body').style.backgroundColor = 'rgba(241,63,247,1)'
+document.querySelector ('body').style.color = 'white'
+}
+
+document.getElementById ('green').onclick = partyGreen 
+function partyGreen() {
+document.querySelector ('body').style.backgroundColor = 'rgba(0,253,81,1)'
+document.querySelector ('body').style.color = 'white'
+}
+ 
+document.getElementById ('blue').onclick = partyBlue
+function partyBlue() {
+document.querySelector ('body').style.backgroundColor = 'rgba(0,254,255)'
+document.querySelector ('body').style.color = 'white'
+}*/
+
+
+/*document.getElementById ('purple').onclick = partyPurple 
+function partyPurple() {
+  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+  document.querySelector('body').style.color = 'white'
+}
+
+document.getElementById ('green').onclick = partyGreen
+function partyGreen () {
+document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
+document.querySelector('body').style.color = 'white'
+}
+
+document.getElementById ('blue').onclick = partyBlue
+function partyBlue () {
+  document.querySelector ('body').style.backgroundColor = 'rgba(0,254,255)'
+  document.querySelector ('body').style.color = 'white'
+}*/
+
+/*document.getElementById ('purple').onclick = partyPurple 
+function partyPurple () {
+  document.querySelector ('body').style.backgroundColor = 'rgba(241,63,247,1)'
+  document.querySelector ('body').style.color = 'white'
+}
+
+document.getElementById ('green').onclick = partyGreen
+function partyGreen () {
+  document.querySelector ('body').style.backgroundColor = 'rgba(0,253,81,1)'
+  document.querySelector ('body').style.color = 'white'
+}
+
+document.getElementById ('blue').onclick = partyBlue
+function partyBlue () {
+  document.querySelector ('body').style.backgroundColor = 'rgba(0,254,255)'
+  document.querySelector ('body'),style.color = 'white'
+}*/
+
+
+document.getElementById ('red').onclick = partyRed
+function partyRed () {
+  document.querySelector ('body').style.backgroundColor = 'red'
+  document.querySelector ('body').style.color = 'white'
+}
+
+document.getElementById('purple').onclick = partyPurple
+function partyPurple () {
+  document.querySelector ('body').style.backgroundColor = 'purple'
+  document.querySelector ('body').style.color = 'white'
+}
+
+document.getElementById('gold').onclick = partyGold
+function partyGold () {
+  document.querySelector ('body').style.backgroundColor = 'gold'
+  document.querySelector ('body').style.color = 'white'
+}
